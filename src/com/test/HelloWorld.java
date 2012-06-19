98aa34dd310ab829db193919973511bfd2b5fc99
yamiao <yaojiamiao2011@gmail.com>
2012-06-19 21:31:00 +0800

    准备提交到库中了

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
